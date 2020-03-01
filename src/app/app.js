6603991c1e35793543f1063da942f1613a8a8c5e
Roberta Melo <robertameelo91@gmail.com>
2020-03-01 15:11:25 -0300

    Created structure inital.

---
+++ src/app/app.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
